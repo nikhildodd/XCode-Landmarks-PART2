d11e88dee89170bd1d7778e8970cfb5d89106ffa
nikhildodd <44418217+nikhildodd@users.noreply.github.com>
2019-11-01 12:32:35 -0700

    Add files via upload
    Create a list view using SwiftUI to display different landmarks.

---
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Nikhil Dodd on 10/30/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView{
+            List (landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                    LandmarkRow(landmark:landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
+    }
+}
